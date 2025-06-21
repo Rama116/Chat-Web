1850b0587494349b2b020d6b502e2bd4151084d3
Rama116 <ramagugan77@gmail.com>
2025-06-21 12:00:47 +0530

    SideBar FrontEnd -Done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -14,9 +14,9 @@ export const HomePage = () => {
        overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 
         'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols[1fr_2fr_1fr]' : 
         'md:grid-cols-2'}`}>
-            <SideBar />
-            <ChatContainer />
-            <RightSideBar />
+            <SideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+            <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+            <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
        </div>
     </div>
   )
