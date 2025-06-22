b9c2e7cb997de8a63b6f66a99f38bb51d9f2ed49
Rama116 <ramagugan77@gmail.com>
2025-06-22 12:36:07 +0530

    cloudinary and auth setup

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,7 @@
 
 // SingUp new User
 
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcrypt.js";
@@ -55,6 +56,38 @@ export const login = async (req, res) => {
         res.json({success: true, userData, token, message: "Login Successful"})
     } catch (error) {
         console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Controller to check if user is authenticated
+export const checkAuth = (req, res) => {
+    res.json({success: true, user: req.user}); // will return user data when authenticated
+}
+
+// controller to update user profile details
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic, bio, fullName} = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+
+        if (!profilePic)
+        {
+            updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName},
+             {new: true})
+        } 
+        else {
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName}, {new: true});
+        }
+
+        res.json({success: true, user: updatedUser})
+    } catch (error) {
+        console.log(error.message)
         res.json({success: false, message: error.message})
     }
 }
