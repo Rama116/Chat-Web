202c898fea1d41470c5e6ec92a555f7b47b7ff43
Rama116 <ramagugan77@gmail.com>
2025-06-22 11:22:08 +0530

    Server and DB setup Done

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+
+// create express app and http server
+
+const app = express()
+const server = http.createServer(app) // it supports socket io
+
+// middleware setup
+
+app.use(express.json({limit: "4mb"})); // img only 4mb limit
+app.use(cors());
+
+app.use("/api/status", (req, res) => res.send("Server is Live"));
+
+// connect to mongoDB
+
+await connectDB();
+
+const PORT = process.env.PORT || 5000; // either choose a port from env var or start in 5000
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
