724ac80f08b428c386cd1906ca7f955e06805cf5
Rama116 <ramagugan77@gmail.com>
2025-06-22 19:07:20 +0530

    Message Backend Done

--- server/controllers/MessageController.js
+++ server/controllers/MessageController.js
@@ -1,5 +1,7 @@
 import Message from "../models/Message.js"
 import User from "../models/User.js"
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
 
 // get all users except the logged in users
 export const getUserForSidebar = async (req, res) => {
@@ -57,6 +59,40 @@ export const markMessageAsSeen = async (req, res) => {
         const {id} = req.params;
         await Message.findByIdAndUpdate(id, {seen: true});
         res.json({success: true})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false,message: error.message});
+    }
+}
+
+// send message to selected user
+export const sendMessage = async (req ,res) => {
+    try {
+        const {text , image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        // emit the new Message to the receivers socket
+        const receiverSocketId = userSocketMap[receiverId]
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("New Message", newMessage);
+        }
+
+        res.json({success: true, newMessage});
+
     } catch (error) {
         console.log(error.message);
         res.json({success: false,message: error.message});
--- server/server.js
+++ server/server.js
@@ -5,11 +5,40 @@ import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/UserRoutes.js";
 import messageRouter from "./routes/MessageRoutes.js";
+import {Server} from "socket.io";
 
 // create express app and http server
 
 const app = express()
 const server = http.createServer(app) // it supports socket io
+
+// Initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+// store online users
+
+export const userSocketMap = {}; // data is stored in form (userId: socketId)
+
+// socket.io connection handler
+
+// all the online users are stored in the above array (kinda)
+io.on("connection" , (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.Id;
+
+    // emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
 
 // middleware setup
 
